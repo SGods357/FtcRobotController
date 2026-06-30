61fa89d775a63d6432805a4344e6a70e0b609d17
SGods357 <shivninza@gmail.com>
2026-06-30 15:34:47 -0700

    Create AprilTagLimelightTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagLimelightTest.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
+public class AprilTagLimelightTest extends OpMode {
+    private Limelight3A limelight3A;
+    private IMU imu;
+
+    @Override
+    public void init() {
+        limelight3A = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight3A.pipelineSwitch(8);
+        imu = hardwareMap.get(IMU.class, "imu");
+        RevHubOrientationOnRobot revHubOrientationOnRobot = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        );
+        imu.initialize(new IMU.Parameters((revHubOrientationOnRobot)));
+    }
+
+    @Override
+    public void start() {
+        limelight3A.start();
+    }
+
+    @Override
+    public void loop() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        limelight3A.updateRobotOrientation(orientation.getYaw());
+        LLResult llResult = limelight3A.getLatestResult();
+        if (llResult != null && llResult.isValid()) {
+            Pose3D botPose = llResult.getBotpose_MT2(); // Only for yaw, only use getBotpose for MT1
+            telemetry.addData("Tx", llResult.getTx());
+            telemetry.addData("Ty", llResult.getTy());
+            telemetry.addData("Ta", llResult.getTa());
+            telemetry.addData("BotPose", botpose.toString());
+            telemetry.addData("Yaw", botPose.getOrientation().getYaw());
+        }
+
+    }
+}
